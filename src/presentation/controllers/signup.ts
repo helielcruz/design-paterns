b78cd0a1e29e72c4a08310679682811ec0db1964
heliel.cruz <heliel.cruz@loviz.com.vc>
2023-06-12 16:03:12 -0300

    feat: ensure SignUpController returns 400 if no password confirmation is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -5,7 +5,7 @@ import { badRequest } from "../helpers/http-helper";
 export class SignUpController {
     handle (httpRequest: any): any {
 
-        const requiredFields = ['name', 'email', 'password']
+        const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
         for(const field of requiredFields){
             if(!httpRequest.body[field]){
